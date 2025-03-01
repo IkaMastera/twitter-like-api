01e5baa4f24712768d32d9c00cd0c5114a44704b
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-03-01 14:08:59 +0400

    add: twitter error handling and error logging

--- twitter_like.py
+++ twitter_like.py
@@ -7,7 +7,6 @@ Usage:
     2. Run: python twitter_like.py <tweet_id>
 """
 
-
 import os
 import logging
 import tweepy
@@ -23,3 +22,15 @@ consumer_secret = os.getenv("TWITTER_COSUMER_SECRET")
 access_token = os.getenv("TWITTER_ACCESS_TOKEN")
 access_secret = os.getenv("TWITTER_ACCESS_SECRET")
 bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
+
+# Checking that all required Twitter cerdentials are provided for error handling
+if not consumer_key or not consumer_secret or not access_token or not access_secret
+    logging.error("Twitter API credentials not fully set in .env. Exiting")
+    exit(1)
+
+# Logs messages to both console and file with timestamps good practice
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.FileHandler("twitter_script.log"), logging.StreamHandler()]
+)
